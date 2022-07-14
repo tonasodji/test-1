0c1d307e433387f60b517c582b2ebaefdcaec091
sodji98 <107852003+sodji98@users.noreply.github.com>
2022-07-14 21:08:28 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,15 @@
 cp -r ../../../../ECLAIR/* ./
-cd ECLAIR
 
-mkdir tmp && cd tmp
-echo "hello-world" > helloWorld.txt
+- cd ECLAIR
+# Prepare for build
+../prepare.sh Debug
+# Analyze the project
+./analyze.sh
+# Make the directory for ECLAIR project database
+ssh eclair-gitlab@localhost "mkdir -p ~/github/$CI_PROJECT_PATH/$CI_JOB_ID && rm -f ~/github/$CI_PROJECT_PATH/last_$CI_COMMIT_BRANCH && ln -s ~/github/$CI_PROJECT_PATH/$CI_JOB_ID ~/github/$CI_PROJECT_PATH/last_$CI_COMMIT_BRANCH"
+# Copy the project database
+scp out/PROJECT.ecd eclair-gitlab@localhost:~/github/$CI_PROJECT_PATH/$CI_JOB_ID
+# Publish ECLAIR report link
+echo "https://eclairit.com:3787/fs/home/eclair-gitlab/github/$CI_PROJECT_PATH/$CI_JOB_ID/PROJECT.ecd"
+Create the ECLAIR badge
+- anybadge --label=ECLAIR --value=default --file=badge.svg
