af48bd1584a585a3587c660a41f2f6042e3c0715
sodji98 <107852003+sodji98@users.noreply.github.com>
2022-07-19 14:44:11 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@ cd ECLAIR
 # Make the directory for ECLAIR project database
 mkdir -p ~/github/$CI_PROJECT_PATH/$CI_JOB_ID && rm -f ~/github/$CI_PROJECT_PATH/last_$CI_COMMIT_BRANCH && ln -s ~/github/$CI_PROJECT_PATH/$CI_JOB_ID ~/github/$CI_PROJECT_PATH/last_$CI_COMMIT_BRANCH
 # Copy the project database
-scp out/PROJECT.ecd ~/github/$CI_PROJECT_PATH/$CI_JOB_ID
+scp out_CRC_16_Release_MC3/.data/PROJECT.ecd ~/github/$CI_PROJECT_PATH/$CI_JOB_ID
 # Publish ECLAIR report link
 echo "https://eclairit.com:3787/fs/home/emelin/github/$CI_PROJECT_PATH/$CI_JOB_ID/PROJECT.ecd"
 #Create the ECLAIR badge
