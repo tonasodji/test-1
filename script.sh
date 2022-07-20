2f1e28cea2d1fb06a35110079fe5b1eb4da24228
sodji98 <107852003+sodji98@users.noreply.github.com>
2022-07-20 13:07:41 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -11,6 +11,15 @@ mkdir -p ~/github/$GITHUB_REPOSITORY/$GITHUB_RUN_NUMBER && rm -f ~/github/$GITHU
 scp out_CRC_16_Release_MC3/.data/PROJECT.ecd ~/github/$GITHUB_REPOSITORY/$GITHUB_RUN_NUMBER
 # Publish ECLAIR report link
 echo "https://eclairit.com:3787/fs/home/emelin/github/$GITHUB_REPOSITORY/$GITHUB_RUN_NUMBER/PROJECT.ecd"
+echo "Home: ${HOME}"
+echo "GITHUB_WORKFLOW: ${GITHUB_WORKFLOW}"
+echo "GITHUB_ACTIONS: ${GITHUB_ACTIONS}"
+echo "GITHUB_ACTOR: ${GITHUB_ACTOR}"
+echo "GITHUB_REPOSITORY: ${GITHUB_REPOSITORY}"
+echo "GITHUB_EVENT_NAME: ${GITHUB_EVENT_NAME}"
+echo "GITHUB_WORKSPACE: ${GITHUB_WORKSPACE}"
+echo "GITHUB_SHA: ${GITHUB_SHA}"
+echo "GITHUB_REF: ${GITHUB_REF}"
 cp report.sarif out_CRC_16_Release_MC3/reports
 #Create the ECLAIR badge
 anybadge --label=ECLAIR --value=default --file=badge.svg
